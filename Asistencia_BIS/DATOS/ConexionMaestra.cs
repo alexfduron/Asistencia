b96b302c545f3a15a053ff20a39c7bb7fd57ef5a
Alex.Duron <alexfduron@gmail.com>
2021-06-24 22:37:09 -0600

    Insertar personal 2

--- Asistencia_BIS/DATOS/ConexionMaestra.cs
+++ Asistencia_BIS/DATOS/ConexionMaestra.cs
@@ -12,9 +12,33 @@ namespace Asistencia_BIS.DATOS
     public class ConexionMaestra
     {
 
-        public static string Conexion = "";
+        public static string Conexion = @"Data Source = BISW_PRODUC19\SQLEXPRESS; Initial Catalog = PERSONAL_BIS; Integrated Security = true";
 
         public static SqlConnection Conectar = new SqlConnection(Conexion);
+
+        public static void Abrir()
+        {
+
+            if(Conectar.State == ConnectionState.Closed)
+            {
+
+                Conectar.Open();
+
+            }
+
+        }
+
+        public static void Cerrar()
+        {
+
+            if(Conectar.State == ConnectionState.Open)
+            {
+
+                Conectar.Close();
+
+            }
+
+        }
 
     }
 }
--- Asistencia_BIS/DATOS/Datos_Personal.cs
+++ Asistencia_BIS/DATOS/Datos_Personal.cs
@@ -10,15 +10,59 @@ using System.Data.SqlClient;
 //importamos la clase Logica
 using Asistencia_BIS.LOGICA;
 
+//importamos la libreria para MsgBox
+using System.Windows.Forms;
+
 namespace Asistencia_BIS.DATO
 {
     class Datos_Personal
     {
 
-        public bool Insertar_Personal(LogicaPersonal parametros)
+        public bool Insertar_Personal(LogicaPersonal Parametros)
         {
 
+            try
+            {
 
+                Asistencia_BIS.DATOS.ConexionMaestra.Abrir();
+
+                SqlCommand Cmd = new SqlCommand("Insertar_Personal", Asistencia_BIS.DATOS.ConexionMaestra.Conectar);
+
+                Cmd.CommandType = CommandType.StoredProcedure;
+
+                Cmd.Parameters.AddWithValue("@Codigo", Parametros.Codigo);
+                Cmd.Parameters.AddWithValue("@Nombre", Parametros.Nombre);
+                Cmd.Parameters.AddWithValue("@Apellido", Parametros.Apellido);
+                Cmd.Parameters.AddWithValue("@ID_Centro_de_Costo", Parametros.ID_Centro_de_Costo);
+                Cmd.Parameters.AddWithValue("@ID_Cargo", Parametros.ID_Cargo);
+                Cmd.Parameters.AddWithValue("@ID_Supervisor", Parametros.ID_Supervisor);
+
+                Cmd.Parameters.AddWithValue("@Foto", Parametros.Foto);
+
+                Cmd.ExecuteNonQuery();
+
+                return true;
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                //ex.Message muestra el resumen del error (se usa para mostrar el mensaje del SQL)
+                //ex.StackTrace muestra el mensaje completo (numero de linea, clase, form, folder, no muestra error SQL)
+                MessageBox.Show(ex.Message);
+
+                return false;
+
+            }
+
+            finally
+            {
+
+                Asistencia_BIS.DATOS.ConexionMaestra.Cerrar();
+
+            }
 
         }
 
